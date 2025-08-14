aa7f53292fd9769eaaef04f6f043c59e70d657cd
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-08-14 12:40:05 +0500

    build fix

--- src/lib/format-text-content.tsx
+++ src/lib/format-text-content.tsx
@@ -9,8 +9,6 @@ export const formatRoutineText = (text: string) => {
     .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
     // Double quotes: "text" => <strong>text</strong> (but not if it's inside a code block)
     .replace(/"([^"]+)"/g, '<strong>$1</strong>')
-    // Code blocks: ```text``` => <code>text</code>
-    .replace(/```(.*?)```/gs, '<code>$1</code>')
-    // For *text* => <strong>text</strong>
+    // STRONG *text* => <strong>text</strong>
     .replace(/\*(.*?)\*/g, '<strong>$1</strong>')
 };
